d82bbf1039b9c53a48d2f1fb54186a8e5f604b7f
it-by-choice <ranglaniaakash@gmail.com>
2020-04-27 15:07:55 +0530

    xml + json support added

--- mobile-webapp-ws/src/main/java/com/mobilewebapp/ws/mobilewebappws/controller/UserController.java
+++ mobile-webapp-ws/src/main/java/com/mobilewebapp/ws/mobilewebappws/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mobilewebapp.ws.mobilewebappws.controller;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,8 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public UserRest createUser(@RequestBody UserRequest userRequest) {
 		System.out.println("Creating a user...");
 
@@ -39,7 +41,7 @@ public class UserController {
 
 	}
 
-	@GetMapping(path = "/{id}")
+	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public UserRest fetchUser(@PathVariable String id) {
 		UserRest userResponse = new UserRest();
 		UserDto userDto = userService.getUserByUserId(id);
