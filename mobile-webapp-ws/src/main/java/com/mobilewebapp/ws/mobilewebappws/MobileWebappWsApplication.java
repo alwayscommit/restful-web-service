6427aa2466a86dd133f2f6e25f7c143227636733
it-by-choice <ranglaniaakash@gmail.com>
2020-05-01 17:02:53 +0530

    added war config

--- mobile-webapp-ws/src/main/java/com/mobilewebapp/ws/mobilewebappws/MobileWebappWsApplication.java
+++ mobile-webapp-ws/src/main/java/com/mobilewebapp/ws/mobilewebappws/MobileWebappWsApplication.java
@@ -2,12 +2,20 @@ package com.mobilewebapp.ws.mobilewebappws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileWebappWsApplication {
+public class MobileWebappWsApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileWebappWsApplication.class);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MobileWebappWsApplication.class, args);
 	}
